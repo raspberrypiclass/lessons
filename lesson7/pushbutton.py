ebec1dd54b664b3a6142b565a252105f304bdaa1
raspberrypiclass <raspberrypiclass@gmail.com>
2014-04-26 19:24:15 +1000

    Update pushbutton.py

--- lesson7/pushbutton.py
+++ lesson7/pushbutton.py
@@ -1,11 +1,20 @@
+#!/usr/bin/env python
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12,GPIO.IN)
+##tell the program which pin will be used to read the state of the button
+pin = 12
 
+## set pin numbering mode to board rather than chip
+GPIO.setmode(GPIO.BOARD)
+## tell the program we're going to be reading from the pin rather than outputting to it
+GPIO.setup(pin,GPIO.IN)
+
+##loop forever
+##at each pass through the loop check the if the button has been pressed
+##if it has output a message
 while True:
-	input_value = GPIO.input(12)
-	if GPIO.input(12)== False:
+	input_value = GPIO.input(pin)
+	if GPIO.input(pin)== False:
 		print("The button has been pressed.")
-		while GPIO.input(12) == False:
+		while GPIO.input(pin) == False:
 			pass
